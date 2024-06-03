13e127544884e355e6464a872e4b13c09cdc895c
Aisulu <aisulu.imankulova@gmail.com>
2024-06-03 13:44:54 -0400

    adding restart button

--- main.js
+++ main.js
@@ -1,28 +1,45 @@
-const bill = document.querySelector('#bill');
-const service = document.querySelector('#service');
-const people = document.querySelector('#people');
-const result = document.querySelector('#result');
-const each = document.querySelector('#each');
-const button = document.querySelector('#calculate');
-const total = document.querySelector('#total');
+const bill = document.querySelector("#bill");
+const service = document.querySelector("#service");
+const people = document.querySelector("#people");
+const result = document.querySelector("#result");
+const each = document.querySelector("#each");
+const button = document.querySelector("#calculate");
+const total = document.querySelector("#total");
+const restartBtn = document.querySelector("#restart");
+const footer = document.querySelector(".footer");
 
-button.addEventListener('click', calculate)
+button.addEventListener("click", calculate);
+restartBtn.addEventListener("click", restart);
 
-function calculate(){
-    let index = service.selectedIndex;
-    let totalTip = Number(bill.value)
-* Number(service.options[index].value)/100
- result.style.visibility = 'visible';
- total.style.visibility = 'visible';
+function calculate() {
+  let index = service.selectedIndex;
+  let totalTip =
+    (Number(bill.value) * Number(service.options[index].value)) / 100;
+  result.style.visibility = "visible";
+  total.style.visibility = "visible";
 
-
-if(people.value>1){
-    result.innerHTML = ((Number(bill.value)+totalTip)/Number(people.value)).toFixed(2)
-    each.style.visibility = 'visible'
-} else {
-    result.innerHTML = (Number(bill.value)+ totalTip).toFixed(2)
-   
+  if (people.value > 1) {
+    result.innerHTML = (
+      (Number(bill.value) + totalTip) /
+      Number(people.value)
+    ).toFixed(2);
+    each.style.visibility = "visible";
+  } else {
+    result.innerHTML = (Number(bill.value) + totalTip).toFixed(2);
+  }
 }
+// restartBtn.addEventListener("click", function () {
+//   bill.value = "";
+//   people.value = "";
+//   result.style.visibility = "hidden";
+//   total.style.visibility = "hidden";
+//   each.style.visibility = "hidden";
+// });
 
-
+function restart() {
+  bill.value = "";
+  people.value = "";
+  result.style.visibility = "hidden";
+  total.style.visibility = "hidden";
+  each.style.visibility = "hidden";
 }
